cb8a8a9f366257400645f81daf6241216461fdec
Jeroen Heemskerk <j.w.heemskerk@gmail.com>
2022-01-25 15:18:23 +0100

    Initial commit

---
+++ OpenApiInterpreter/OpenApiInterface.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.OpenApi;
+using Microsoft.OpenApi.Extensions;
+using Microsoft.OpenApi.Models;
+using Microsoft.OpenApi.Readers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace OpenApiInterpreter
+{
+    public class OpenApiInterface
+    {
+        protected OpenApiDocument openApiDocument { get; private set; } = new OpenApiDocument();
+
+        private OpenApiDiagnostic diagnostic = new OpenApiDiagnostic();
+        /// <summary>
+        /// Convenicence property
+        /// </summary>
+        public bool HasErrors
+        {
+            get
+            {
+                return diagnostic.Errors.Count > 0;
+            }
+        }
+        /// <summary>
+        /// Convenience property
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<string> Errors
+        {
+            get
+            {
+                return diagnostic.Errors.Select(oae => oae.Message);
+            }
+        }
+        public async Task ReadAsync(string openApiDefinition)
+        {
+            await Task.Run(() =>
+            {
+                using (var httpClient = new HttpClient())
+                {
+                    try
+                    {
+                        var stream = httpClient.GetStreamAsync(openApiDefinition);
+
+                        // Read V3 as YAML
+                        openApiDocument = new OpenApiStreamReader().Read(stream.Result, out diagnostic);
+                    }
+                    catch (AggregateException e)
+                    {
+                        diagnostic.Errors.Add(new OpenApiError("HTTP", e.InnerException.Message));
+                    }
+                }
+            });
+        }
+
+        public List<string> GetPaths()
+        {
+            return openApiDocument.Paths.Select(openApiPathItem => openApiPathItem.Key).ToList();
+        }
+
+        internal IDictionary<OperationType, OpenApiOperation> GetMethods(string path)
+        {
+            return openApiDocument.Paths[path].Operations;
+        }
+
+        public void WriteAsJson()
+        {
+            // Write V2 as JSON
+            var outputString = openApiDocument.Serialize(OpenApiSpecVersion.OpenApi2_0, OpenApiFormat.Json);
+
+            Console.WriteLine(outputString);
+        }
+    }
+}
---
+++ OpenApiInterpreter/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenApiInterpreter
+{
+    internal class Program
+    {
+        private const string ExampleOpenAPDefinitionV3Yaml = "https://raw.githubusercontent.com/OAI/OpenAPI-Specification/master/examples/v3.0/petstore.yaml";
+        private const string ExampleOpenAPDefinitionV3Json = "https://raw.githubusercontent.com/OAI/OpenAPI-Specification/master/examples/v3.0/petstore.json";
+
+        static void Main(string[] args)
+        {
+            var openApiIntf = new OpenApiInterface();
+
+            openApiIntf.ReadAsync(ExampleOpenAPDefinitionV3Json).GetAwaiter().GetResult();
+
+            if (openApiIntf.HasErrors)
+            {
+                Console.WriteLine("Found errors: ");
+                foreach (var error in openApiIntf.Errors)
+                {
+                    Console.WriteLine(error);
+                }
+                Console.ReadKey();
+                return;
+            }
+
+            Console.WriteLine("Paths:");
+            foreach (var path in openApiIntf.GetPaths()) {
+                Console.WriteLine(path);
+
+                Console.WriteLine("  Methods:");
+                foreach(var method in openApiIntf.GetMethods(path))
+                {
+                    Console.WriteLine($"    { method.Key.ToString().ToUpper() }: {method.Value.Summary} ");
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
